54d637e11dfa9f62ae8cc100facd3bb4a5d20a25
Izaack Kuria <kuriaisac@gmail.com>
2024-06-29 10:47:55 +0300

    Objects

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name: "Soka",
+    streetAddress: "China", 
+}
+//function to  return a new Object that has an updated value for the key passed in
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee, [key]:value
+    }
+}
+//function to mutate the employee Object passed in
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+   employee[key] = value;
+   return employee;
+}
+//function to  delete the property with that key from the employee Object
+function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = {...employee};
+    delete updatedEmployee[key];
+    return updatedEmployee;
+} 
+//function to delete the property but mutate the employee Object
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+} 
